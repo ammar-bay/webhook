a212533242cd1659e66f59aceff586a24f780e5c
Muhammad Ammar Ibrahim <23110345@lums.edu.pk>
2022-10-29 04:44:26 +0500

    ok

--- routes/conversations.js
+++ routes/conversations.js
@@ -1,47 +1,45 @@
 const Conversation = require("../models/Conversation");
 
 //new conv
-const ConversationRouter = (io)=> {
+const ConversationRouter = (io) => {
   const router = require("express").Router();
-  
-  router.post("/", async (req, res) => {
-    const newConversation = new Conversation({
-      members: [req.body.senderId, req.body.receiverId],
+
+  //   router.post("/", async (req, res) => {
+  //     const newConversation = new Conversation({
+  //       members: [req.body.senderId, req.body.receiverId],
+  //   });
+
+  //   try {
+  //     const savedConversation = await newConversation.save();
+  //     res.status(200).json(savedConversation);
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  // });
+
+  //get conv of a user
+
+  router.get("/", async (req, res) => {
+    try {
+      const conversation = await Conversation.find();
+      res.status(200).json(conversation);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
-  
-  try {
-    const savedConversation = await newConversation.save();
-    res.status(200).json(savedConversation);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
-//get conv of a user
+  // get conv includes two userId
 
-router.get("/:userId", async (req, res) => {
-  try {
-    const conversation = await Conversation.find({
-      members: { $in: [req.params.userId] },
-    });
-    res.status(200).json(conversation);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// get conv includes two userId
-
-router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
-  try {
-    const conversation = await Conversation.findOne({
-      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
-    });
-    res.status(200).json(conversation)
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-return router
-}
+  router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
+    try {
+      const conversation = await Conversation.findOne({
+        members: { $all: [req.params.firstUserId, req.params.secondUserId] },
+      });
+      res.status(200).json(conversation);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  return router;
+};
 module.exports = ConversationRouter;
