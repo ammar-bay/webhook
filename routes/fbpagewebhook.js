ce367bbefef02d05fc728d0e4c029be70a187e5f
Muhammad Ammar Ibrahim <23110345@lums.edu.pk>
2022-10-30 01:32:39 +0500

    yes

--- routes/fbpagewebhook.js
+++ routes/fbpagewebhook.js
@@ -2,54 +2,29 @@ const axios = require("axios");
 
 const FacebookWebhookRouter = (io) => {
   const router = require("express").Router();
-  router.post("/", async (req, res) => {
-    // Parse the request body from the POST
-    //   let body = req.body;
 
+  // getting notification from facebook
+  router.post("/", async (req, res) => {
     // Check the Incoming webhook message
     console.log(JSON.stringify(req.body, null, 2));
-    io.emit("fbEvents", req.body);
 
-    // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-    // if (req.body.object) {
-    //   if (
-    //     req.body.entry &&
-    //     req.body.entry[0].changes &&
-    //     req.body.entry[0].changes[0] &&
-    //     req.body.entry[0].changes[0].value.messages &&
-    //     req.body.entry[0].changes[0].value.messages[0]
-    //   ) {
-    //     let phone_number_id =
-    //       req.body.entry[0].changes[0].value.metadata.phone_number_id;
-    //     let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-    //     let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-    //     console.log("Sending axios post");
-    //     try {
-    //       const result = await axios({
-    //         method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-    //         url:
-    //           "https://graph.facebook.com/v12.0/" +
-    //           phone_number_id +
-    //           "/messages?access_token=" +
-    //           process.env.WA_ACCESS_TOKEN,
-    //         data: {
-    //           messaging_product: "whatsapp",
-    //           to: from,
-    //           text: { body: "Ack: " + msg_body },
-    //         },
-    //         headers: { "Content-Type": "application/json" },
-    //       });
-    //     } catch (error) {
-    //       console.log(error);
-    //       console.log("ERROR!!");
-    //     }
-    //   }
+    if (req.body.object === "page") {
+      if (
+        req.body.entry &&
+        req.body.entry[0].changes &&
+        req.body.entry[0].changes[0] &&
+        req.body.entry[0].changes[0].value.item
+      ) {
+        // const type = req.body.entry[0].changes[0].value.item;
+        const value = req.body.entry[0].changes[0].value;
 
+        io.emit("fbEvents", value);
+      }
       res.sendStatus(200);
-    // } else {
-    //   // Return a '404 Not Found' if event is not from a WhatsApp API
-    //   res.sendStatus(404);
-    // }
+    } else {
+      // Return a '404 Not Found' if event is not from a WhatsApp API
+      res.sendStatus(404);
+    }
   });
 
   // Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
