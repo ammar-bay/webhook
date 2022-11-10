e58a76cc9de5e81b58865a357c3febb4f075f007
Muhammad Ammar Ibrahim <23110345@lums.edu.pk>
2022-11-11 00:03:33 +0500

    ok

--- routes/auth.js
+++ routes/auth.js
@@ -18,26 +18,30 @@ router.post("/register", async (req, res) => {
 
     //save user and respond
     const user = await newUser.save();
+    console.log("sign up request successful");
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
-    console.log("login req");
     const user = await User.findOne({ email: req.body.email });
-    console.log(user);
+    // console.log(user);
     !user && res.status(404).json("user not found");
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("wrong password")
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
 
-    res.status(200).json(user)
+    console.log("login req successful");
+    res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
