5ec2d254f48d73756d2cbdb484ed2a0dede50f77
Muhammad Ammar Ibrahim <23110345@lums.edu.pk>
2022-11-16 16:48:44 +0500

    aa

--- routes/messages.js
+++ routes/messages.js
@@ -25,7 +25,7 @@ router.post("/", async (req, res) => {
     const result = await axios.post(url, body, {
       headers: { Authorization: token, "Content-Type": "application/json" },
     });
-    // console.log(result.data);
+    console.log(result.data.messages[0].id);
     const newMessage = new Message({
       ...req.body,
       id: result.data.messages[0].id,
