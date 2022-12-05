cb39253478f749ff558fd944442b6f31ba46e3d8
Muhammad Ammar Ibrahim <23110345@lums.edu.pk>
2022-12-05 17:33:51 +0500

    done

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/unifiedchat-server
+sudo chmod -R 777 /home/ubuntu/unifiedchat-server
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/unifiedchat-server
+cd /home/ubuntu/unifiedchat-server
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
