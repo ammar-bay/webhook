8b707f1f1cc4c2f6fb5432928828b114d13afc65
Muhammad Ammar Ibrahim <23110345@lums.edu.pk>
2022-12-05 17:41:19 +0500

    ok

--- routes/root.js
+++ routes/root.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   console.log("root route");
-  res.send("Hello World!");
+  res.send("Hello World! This is UC");
 });
 
 module.exports = router;
