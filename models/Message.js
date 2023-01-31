1989c2865b62b5c62451da6fc9ac745918182baa
Muhammad Ammar Ibrahim <23110345@lums.edu.pk>
2023-01-31 13:58:28 +0500

    s

--- models/Message.js
+++ models/Message.js
@@ -26,7 +26,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       content: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT("medium"),
         allowNull: false,
       },
       status: {
